83f2e9011b90bfb67df9ea9d4f89f4fee9a24e67
xoddlxod21 <125775019+xoddlxod21@users.noreply.github.com>
2023-06-11 16:03:28 -0500

    Time: 65 ms (28.76%), Space: 41.6 MB (89.56%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let first=m-1;
+    let second=n-1;
+    let ith=m+n-1;
+    
+   while(second>=0){
+        let firstval=nums1[first];
+        let secondval=nums2[second];
+        if(firstval>secondval){
+            nums1[ith]=firstval
+            ith--
+            first--
+        }else{
+            nums1[ith]=secondval
+            ith--
+            second--
+        }
+    }
+};
